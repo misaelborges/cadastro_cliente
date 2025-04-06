c8adb37b218726b015854f4bb63d579f10abaf13
Mizael <mizaelborges2011@gmail.com>
2025-04-06 10:40:32 -0300

    adicionando um metodo para converte o ClienteRequestAtualizacaoDTO para um cliente, e outro para converte um CLienteResponseResumoDTO para um cliente

--- src/main/java/com/useall/cadastro_cliente/api/ClienteResponseAssembler.java
+++ src/main/java/com/useall/cadastro_cliente/api/ClienteResponseAssembler.java
@@ -1,6 +1,8 @@
 package com.useall.cadastro_cliente.api;
 
 import com.useall.cadastro_cliente.api.dto.ClienteResponseDTO;
+import com.useall.cadastro_cliente.api.dto.ClienteResponseResumoDTO;
+import com.useall.cadastro_cliente.api.dto.ClienteResquestAtualizacaoDTO;
 import com.useall.cadastro_cliente.domain.model.Cliente;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +20,15 @@ public class ClienteResponseAssembler {
         return modelMapper.map(cliente, ClienteResponseDTO.class);
     }
 
-    public List<ClienteResponseDTO> toCollectionModel(List<Cliente> clientes) {
-        return clientes.stream().map(this::ToModel).toList();
+    public ClienteResponseResumoDTO ToModelResumo(Cliente cliente) {
+        return modelMapper.map(cliente, ClienteResponseResumoDTO.class);
+    }
+
+    public ClienteResquestAtualizacaoDTO toModelAtualizacao(Cliente cliente) {
+        return modelMapper.map(cliente, ClienteResquestAtualizacaoDTO.class);
+    }
+
+    public List<ClienteResponseResumoDTO> toCollectionModel(List<Cliente> clientes) {
+        return clientes.stream().map(this::ToModelResumo).toList();
     }
 }
