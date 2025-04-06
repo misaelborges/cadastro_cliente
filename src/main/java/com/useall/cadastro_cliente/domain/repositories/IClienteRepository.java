834e8f35fd50ef69b6911b3e8c8b88e522ba3830
Mizael <mizaelborges2011@gmail.com>
2025-04-06 11:37:26 -0300

    criado metodo para listar apenas os clientes ativos

--- src/main/java/com/useall/cadastro_cliente/domain/repositories/IClienteRepository.java
+++ src/main/java/com/useall/cadastro_cliente/domain/repositories/IClienteRepository.java
@@ -4,7 +4,11 @@ import com.useall.cadastro_cliente.domain.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByAtivoTrue();
 
 }
