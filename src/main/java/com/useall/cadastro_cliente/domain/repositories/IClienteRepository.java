4e8125620d5080df80431c79e2a29fefd5b73a31
Mizael <mizaelborges2011@gmail.com>
2025-04-06 15:23:10 -0300

    metodo para retornar um cliente caso exista um cnpj correspondente

--- src/main/java/com/useall/cadastro_cliente/domain/repositories/IClienteRepository.java
+++ src/main/java/com/useall/cadastro_cliente/domain/repositories/IClienteRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IClienteRepository extends JpaRepository<Cliente, Long> {
 
     List<Cliente> findByAtivoTrue();
+    Optional<Cliente> findByCnpj(String cnpj);
 
 }
