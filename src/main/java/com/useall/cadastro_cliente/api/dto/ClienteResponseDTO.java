f49923a2b6b1d6b9d50663854564140d48479f9a
Mizael <mizaelborges2011@gmail.com>
2025-04-06 10:36:33 -0300

    criado classe DTO para representar todos os atributos do cliente quando o metodo buscarPorId for chamado

--- src/main/java/com/useall/cadastro_cliente/api/dto/ClienteResponseDTO.java
+++ src/main/java/com/useall/cadastro_cliente/api/dto/ClienteResponseDTO.java
@@ -1,24 +1,24 @@
 package com.useall.cadastro_cliente.api.dto;
 
+import java.time.LocalDate;
+
 public class ClienteResponseDTO {
 
-    private Long id;
     private String nome;
+    private String cnpj;
+    private LocalDate dataCadastro;
+    private String endereco;
+    private String telefone;
 
     public ClienteResponseDTO() {
     }
 
-    public ClienteResponseDTO(Long id, String nome) {
-        this.id = id;
+    public ClienteResponseDTO(String nome, String cnpj, LocalDate dataCadastro, String endereco, String telefone) {
         this.nome = nome;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        this.cnpj = cnpj;
+        this.dataCadastro = dataCadastro;
+        this.endereco = endereco;
+        this.telefone = telefone;
     }
 
     public String getNome() {
@@ -27,5 +27,37 @@ public class ClienteResponseDTO {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 }
