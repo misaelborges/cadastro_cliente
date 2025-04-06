5b13367df3d9029e34c3cde7fe0100aa371db914
Mizael <mizaelborges2011@gmail.com>
2025-04-05 23:31:21 -0300

    Refatorando a classe ClienteController para que os metodos buscarCliente e buscarClientePorId comece a usar os DTOs

--- src/main/java/com/useall/cadastro_cliente/api/controllers/ClienteController.java
+++ src/main/java/com/useall/cadastro_cliente/api/controllers/ClienteController.java
@@ -1,11 +1,15 @@
 package com.useall.cadastro_cliente.api.controllers;
 
 import com.useall.cadastro_cliente.api.ClienteRequestDisassembler;
+import com.useall.cadastro_cliente.api.ClienteResponseAssembler;
 import com.useall.cadastro_cliente.api.dto.ClienteRequestDTO;
+import com.useall.cadastro_cliente.api.dto.ClienteResponseDTO;
 import com.useall.cadastro_cliente.domain.model.Cliente;
 import com.useall.cadastro_cliente.domain.services.ClienteService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,14 +26,22 @@ public class ClienteController {
     @Autowired
     private ClienteRequestDisassembler clienteRequestDisassembler;
 
+    @Autowired
+    private ClienteResponseAssembler clienteResponseAssembler;
+
     @GetMapping
-    public ResponseEntity<List<Cliente>> bucarCliente() {
-        return ResponseEntity.status(200).body(service.bucarCliente());
+    @ResponseStatus(HttpStatus.OK)
+    public List<ClienteResponseDTO> bucarCliente() {
+        List<Cliente> clienteList = service.bucarCliente();
+        List<ClienteResponseDTO> clienteResponseDTOS = clienteResponseAssembler.toCollectionModel(clienteList);
+        return clienteResponseDTOS;
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<?> buscarClientePorId(@PathVariable Long id) {
-        return ResponseEntity.status(200).body(service.buscarClientePorId(id));
+    @ResponseStatus(HttpStatus.OK)
+    public ClienteResponseDTO buscarClientePorId(@PathVariable Long id) {
+        Cliente cliente = service.buscarClientePorId(id);
+        return clienteResponseAssembler.ToModel(cliente);
     }
 
     @PostMapping
